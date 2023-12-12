f534c95c34f52d42359b3d09c5f0de32aba6774e
AndreyGRE <andrey.grenev@mail.ru>
2023-12-12 21:12:44 +0300

    delete comm

--- js.js
+++ js.js
@@ -30,14 +30,13 @@ class MapGenerator {
     for (let i = 0; i < this.mapHeight; i++) {
       let row = [];
       for (let j = 0; j < this.mapWidth; j++) {
-        row.push({ type: 1 }); // 1 - стена, 0 - проход
+        row.push({ type: 1 }); 
       }
       this.map.push(row);
     }
   }
 
   generateRooms() {
-    // Генерация случайного количества комнат
     const rooms = [];
     const numOfRooms =
       Math.floor(Math.random() * (this.maxRooms - this.minRooms + 1)) +
@@ -55,13 +54,12 @@ class MapGenerator {
         Math.floor(Math.random() * (this.mapHeight - roomHeight - 1)) + 1;
       for (let j = y; j < y + roomHeight; j++) {
         for (let k = x; k < x + roomWidth; k++) {
-          this.map[j][k] = { type: 0 }; // 0 - проход (комната)
+          this.map[j][k] = { type: 0 };
         }
       }
       rooms.push({ x, y, width: roomWidth, height: roomHeight });
     }
 
-    // Добавление проходов между комнатами
     for (let i = 0; i < rooms.length - 1; i++) {
       let room1 = rooms[i];
       let room2 = rooms[i + 1];
@@ -93,84 +91,70 @@ class MapGenerator {
     for (let i = 0; i < numOfHorizontalPassages; i++) {
       const passageY = Math.floor(Math.random() * (this.mapHeight - 3)) + 1;
 
-      // Расположение прохода по вертикали
       for (let j = 0; j < this.mapWidth; j++) {
-        this.map[passageY][j] = { type: 0 }; // 0 - проход
+        this.map[passageY][j] = { type: 0 }; 
       }
     }
 
     for (let i = 0; i < numOfVerticalPassages; i++) {
       const passageX = Math.floor(Math.random() * (this.mapWidth - 3)) + 1;
-      // Расположение прохода по горизонтали
       for (let j = 0; j < this.mapHeight; j++) {
-        this.map[j][passageX] = { type: 0 }; // 0 - проход
+        this.map[j][passageX] = { type: 0 }; 
       }
     }
   }
 
     generateObjects(){
-        // Генерация случайного количества мечей 2
-        const swords = [];
-        const numOfSwords = 2;
-        let placedSwords = 0;
+      const swords = [];
+      const numOfSwords = 2;
+      let placedSwords = 0;
 
-        while (placedSwords < numOfSwords) {
-            const x = Math.floor(Math.random() * this.mapWidth);
-            const y = Math.floor(Math.random() * this.mapHeight);
-            if (this.map[y][x].type === 0) {
-                // Проверка, что выбранная клетка пустая
-                this.map[y][x] = { type: 2, prop: 20 }; // 2 - меч
-                swords.push({ x, y });
-                placedSwords++;
-            }
-        }
+      while (placedSwords < numOfSwords) {
+          const x = Math.floor(Math.random() * this.mapWidth);
+          const y = Math.floor(Math.random() * this.mapHeight);
+          if (this.map[y][x].type === 0) {
+              this.map[y][x] = { type: 2, prop: 20 }; 
+              swords.push({ x, y });
+              placedSwords++;
+          }
+      }
 
-        // Генерация случайного количества зелий 3
+      const potions = [];
+      const numOfPotions = 10;
+      let placedPotions = 0;
 
-        const potions = [];
-        const numOfPotions = 10;
-        let placedPotions = 0;
+      while (placedPotions < numOfPotions) {
+          const x = Math.floor(Math.random() * this.mapWidth);
+          const y = Math.floor(Math.random() * this.mapHeight);
+          if (this.map[y][x].type === 0) {
+              this.map[y][x] = { type: 3, prop: 20 };
+              potions.push({ x, y });
+              placedPotions++;
+          }
+      }
 
-        while (placedPotions < numOfPotions) {
-            const x = Math.floor(Math.random() * this.mapWidth);
-            const y = Math.floor(Math.random() * this.mapHeight);
-            if (this.map[y][x].type === 0) {
-                // Проверка, что выбранная клетка пустая
-                this.map[y][x] = { type: 3, prop: 20 }; // 3 - зелье
-                potions.push({ x, y });
-                placedPotions++;
-            }
-        }
+      const opponent = [];
+      const numOfOpponent = 10;
+      let placedOpponent = 0;
 
-        // Генерация противники 5
-
-        const opponent = [];
-        const numOfOpponent = 10;
-        let placedOpponent = 0;
-
-        while (placedOpponent < numOfOpponent) {
-            const x = Math.floor(Math.random() * this.mapWidth);
-            const y = Math.floor(Math.random() * this.mapHeight);
-            if (this.map[y][x].type === 0) {
-                // Проверка, что выбранная клетка пустая
-                this.map[y][x] = { type: 5, HP: 100 }; // 5 - противник
-                opponent.push({ x, y });
-                placedOpponent++;
-            }
-        }
-        // Генерация героя и его расположения 4
-        // ебать эти переменные нужны ниже ***************************** 
-        do {
-            this.heroX = Math.floor(Math.random() * this.mapWidth);
-            this.heroY = Math.floor(Math.random() * this.mapHeight);
-        } while (this.map[this.heroY][this.heroX].type !== 0); // Пока не найдем пустую клетку для размещения героя
-        {this.map[this.heroY][this.heroX] = { type: 4, HP: 100, atack: 20 };} // 4 - герой
-        return [this.heroX , this.heroY]
-        
+      while (placedOpponent < numOfOpponent) {
+          const x = Math.floor(Math.random() * this.mapWidth);
+          const y = Math.floor(Math.random() * this.mapHeight);
+          if (this.map[y][x].type === 0) {
+              this.map[y][x] = { type: 5, HP: 100 }; 
+              opponent.push({ x, y });
+              placedOpponent++;
+          }
+      }
+      do {
+          this.heroX = Math.floor(Math.random() * this.mapWidth);
+          this.heroY = Math.floor(Math.random() * this.mapHeight);
+      } while (this.map[this.heroY][this.heroX].type !== 0); 
+      {this.map[this.heroY][this.heroX] = { type: 4, HP: 100, atack: 20 };} 
+      return [this.heroX , this.heroY]
     }        
     
     getMap(){
-    // обновление карты
         const mapElement = document.querySelector(".field");
         mapElement.innerHTML = "";
         for (let i = 0; i < this.mapHeight; i++) {
@@ -205,7 +189,6 @@ class MapGenerator {
                 mapElement.appendChild(cell);
             }
         }
-        // для отладки
         return this.map;
     }
 
@@ -228,10 +211,10 @@ class ActionsHeros {
         let HeroInfo;
 
         const adjacentCells = [
-            [this.heroX, this.heroY - 1], // Верхняя клетка
-            [this.heroX, this.heroY + 1], // Нижняя клетка
-            [this.heroX - 1, this.heroY], // Левая клетка
-            [this.heroX + 1, this.heroY], // Правая клетка
+            [this.heroX, this.heroY - 1],
+            [this.heroX, this.heroY + 1],
+            [this.heroX - 1, this.heroY],
+            [this.heroX + 1, this.heroY],
           ];
     
         switch (this.key) {
@@ -297,11 +280,9 @@ class ActionsHeros {
                     y < this.mapHeight &&
                     this.map[y][x].type === 5
                     ) {
-                    // Противник на соседней клетке, атакуем его
                     this.map[y][x].HP = this.map[y][x].HP - this.map[heroY][heroX].atack;
                         if (this.map[y][x].HP <= 0) {
                             this.map[y][x] = { type: 0 };
-                            // Удаляем противника
                         }
                     }
                 }
@@ -318,7 +299,6 @@ class ActionsHeros {
         mapGenerator.getMap()
         return [this.heroX , this.heroY]
     }
-
 }
 
 class ActionOpponent{
@@ -333,33 +313,31 @@ class ActionOpponent{
         for (let y = 0; y < this.mapHeight; y++) {
                 for (let x = 0; x < this.mapWidth; x++) {
                   if (this.map[y][x].type === 5) {
-                    // Проверяем, что это клетка с противником
-                    // Реализация случайного передвижения противника
-                    let randomDirection = Math.floor(Math.random() * 4); // Генерируем случайное направление (0 - вверх, 1 - вниз, 2 - влево, 3 - вправо)
+                    let randomDirection = Math.floor(Math.random() * 4);
                     let newX = x;
                     let newY = y;
                     let opponentInfo;
                     if (randomDirection === 0 && y > 0 && this.map[y - 1][x].type === 0) {
-                      newY = y - 1; // Движение вверх
+                      newY = y - 1;
                     } else if (
                       randomDirection === 1 &&
                       y < this.mapHeight - 1 &&
                       this.map[y + 1][x].type === 0
                     ) {
-                      newY = y + 1; // Движение вниз
+                      newY = y + 1; 
                     } else if (randomDirection === 2 && x > 0 && this.map[y][x - 1].type === 0) {
-                      newX = x - 1; // Движение влево
+                      newX = x - 1; 
                     } else if (
                       randomDirection === 3 &&
                       x < this.mapWidth - 1 &&
                       this.map[y][x + 1].type === 0
                     ) {
-                      newX = x + 1; // Движение вправо
+                      newX = x + 1;
                     }
                     if (newX !== x || newY !== y) {
                       opponentInfo = { ...this.map[y][x] };
-                      this.map[y][x] = { type: 0 }; // Очищаем текущую позицию противника
-                      this.map[newY][newX] = { ...opponentInfo }; // Обновляем позицию противника на карте
+                      this.map[y][x] = { type: 0 }; 
+                      this.map[newY][newX] = { ...opponentInfo }; 
                     }
                 }
             }
@@ -370,13 +348,11 @@ class ActionOpponent{
         for (let y = 0; y < this.mapHeight; y++) {
                 for (let x = 0; x < this.mapWidth; x++) {
                   if (this.map[y][x].type === 5) {
-                    // Проверяем, что это клетка с противником
-                    // Реализация атаку вокруг
                     const adjacentCells = [
-                      [x, y - 1], // Верхняя клетка
-                      [x, y + 1], // Нижняя клетка
-                      [x - 1, y], // Левая клетка
-                      [x + 1, y], // Правая клетка
+                      [x, y - 1], 
+                      [x, y + 1], 
+                      [x - 1, y], 
+                      [x + 1, y], 
                     ];
                     for (let i = 0; i < adjacentCells.length; i++) {
                       const x = adjacentCells[i][0];
@@ -388,14 +364,12 @@ class ActionOpponent{
                         y < this.mapHeight &&
                         this.map[y][x].type === 4
                       ) {
-                        // Герой на соседней клетке, атакуем его
                         this.map[heroY][heroX].HP = this.map[heroY][heroX].HP - 10;
                         if (this.map[heroY][heroX].HP <= 0) {
                             this.map[heroY][heroX] = { type: 0 };
                             heroY = null;
                             heroX = null;
                             alert("УМЕР");
-                            // Удаляем противника
                         }
                       }
                     }
@@ -426,7 +400,6 @@ map = mapGenerator.getMap();
 heroX = XY[0];
 heroY = XY[1];
 
-// передвижение и удары Героя
 document.addEventListener("keydown", function (e){
     const key = e.key.toLowerCase();
     const actionsHeros = new ActionsHeros(key,heroX,heroY,map,mapWidth,mapHeight)
@@ -434,14 +407,12 @@ document.addEventListener("keydown", function (e){
     heroX = XY[0];
     heroY = XY[1];
 })
-// передвижение и удары Противников и условие победы
 const game = setInterval(() => {
     const actionOpponent = new ActionOpponent(mapHeight,mapWidth,map,heroX,heroY)
     actionOpponent.moveOpponent()
     setTimeout(() => {
        actionOpponent.attackOponent()
     }, 1500);
-    // победа
     let victory = false;
     let loss = true;
     for (let i = 0; i < map.length; i++) {
